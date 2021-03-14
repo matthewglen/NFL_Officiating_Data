65331b08cab0f1f7d76929f36a7114122a8515a2
Matt <36071737+matthewglen@users.noreply.github.com>
2021-03-14 23:54:49 +0000

    Start of web scraping
    - Officials scrapped for all games from 1999-2004.
    - Finish the rest tomorrow (15/03/21)

--- scraping_PFR.R
+++ scraping_PFR.R
@@ -11,27 +11,40 @@ library(stringr)
 # Functions ####
 ## Get official data for a given game ID ####
 # PFF uses yyyymmddXYZ as game ID. Where XYZ is the home team
-getOfficials <- function(gameID) {
-  url <- gsub(" ", "", paste("https://www.pro-football-reference.com/boxscores/",gameID,".htm"))
+getOfficials <- function(pff_full_ID) {
+  # URL of desired game
+  url <- gsub(
+    " ", "", paste(
+      "https://www.pro-football-reference.com/boxscores/",pff_full_ID,".htm"
+      )
+    )
   url %>% read_html() %>% html_nodes(xpath = '//comment()') %>%    # select comments
     html_text() %>%    # extract comment text
     paste(collapse = '') %>%  # collapse to single string
     read_html() %>%  # reread as HTML
-    html_node('table#officials') %>%    # select desired node
-    html_table(1)
+    html_node('table#officials') %>%    # select officials node
+    html_table(1)    # output as tibble
 }
 
-getPositions <-function(gameID) {
+# Use the given gameID, and the getOfficials function
+getPositions <-function(game_id, pff_full_ID) {
+  # Empty new line
   newLine <- ""
+  newLine <- paste(newLine,game_id,",", sep = "")
+  # Call getOfficials and iterate through the officials table
   for (i in 1:7) {
-    official <- as.data.frame(getOfficials(gameID))[i,2]
+    official <- as.data.frame(getOfficials(pff_full_ID))[i,2]
+    # Add each official to a new line followed by a comma
     newLine <- paste(newLine,official,",", sep = "")
   }
+  # Remove the last comma from the line
   newLine <- str_sub(newLine,1,nchar(newLine)-1)
-  print(newLine)
+  # Write the new line to the csv file
+  write.table(newLine, 
+            "/Users/mattglen/Documents/NFL Analysis/officials_by_game.csv", 
+            append = TRUE, quote = FALSE, sep = ",", eol = "\n", na = "NA", 
+            row.names = FALSE, col.names = FALSE)
 }
-
-getPositions("200709060clt")
 
 # Get game data ####
 # Lee Sharp's games file
@@ -42,9 +55,10 @@ gameIDs <- games %>%
   select(game_id, away_team, home_team, old_game_id)
 gameIDs %>% head(5)
 
-# Check which home tead codes are used in the data
+# Check which home team codes are used in the data
 print(distinct(gameIDs, home_team), n=50)
 
+# Create the PFR version of game ID for each game ####
 # Create a new DF to calculate and store the PFF ID
 pffIDs <- gameIDs %>%
   mutate(
@@ -75,6 +89,7 @@ pffIDs <- pffIDs %>%
       pffID_ch == 'la' ~ 'ram',
       pffID_ch == 'lac' ~ 'sdg',
       pffID_ch == 'lv' ~ 'rai',
+      pffID_ch == 'tb' ~ 'tam',
       TRUE ~ pffID_ch
     )
   )
@@ -88,3 +103,28 @@ pffIDs <- pffIDs %>%
 
 # Check pffIDs
 pffIDs %>% head(5)
+pffIDs %>% tail(5)
+
+# Call the functions for each game ####
+# Check row count
+nrow(pffIDs)
+
+# For each row in the file...
+for (c in 1579:nrow(pffIDs)) {
+  #...call the getPositions function. Passing the game_id and pfr_full_id
+  # of the given row
+  getPositions(pffIDs[c,1], pffIDs[c,7])
+}
+
+# Check a specific row if there's a 404 error
+pffIDs [1578,]
+# Sometimes, usually for the super bowl games, PFR has the wrong team listed at
+# home. A check of wikipedia for each error show's Lee's 'games' data is 
+# correct. Not sure why PFR has it like this, but a quick edit of the data
+# sorts it all out.
+# Line 1044 - OAK/TB SB in '03. Need to change from 200301260tam to 200301260rai
+pffIDs[1044,7]="200301260rai"
+# Line 1311 - CAR/PAT in '04. Need to change from 200402010nwe to 200402010car
+pffIDs[1311,7]="200402010car"
+# Line 1578 - PAT/PHI in '05. Need to change from 200502060phi to 200502060nwe
+pffIDs[1578,7]="200502060nwe"
